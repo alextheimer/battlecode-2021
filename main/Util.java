99ae537eff24f65a9306735366df6dce8c96a3b8
Alex Theimer <theimer@mit.edu>
2021-01-02 05:44:11 -0500

    Added missing static keyword in Util.java.

--- main/Util.java
+++ main/Util.java
@@ -9,7 +9,7 @@ public class Util {
 	/**
 	 * Immutable struct-like class for storage of 2D (x, y) coordinate.
 	 */
-	public class IntCoord {
+	public static class IntCoord {
 		public final int x, y;
 		public IntCoord(int x, int y) {
 			this.x = x;
