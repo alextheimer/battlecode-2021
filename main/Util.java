33bea10d6911f163d409a5d52a01f2df47e406c6
Alex Theimer <theimer@mit.edu>
2021-01-02 04:07:40 -0500

    Override equals/hashCode in Util.IntCoord.

--- main/Util.java
+++ main/Util.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Objects;
+
 /**
  * Contains utility functions/classes.
  */
@@ -12,6 +14,17 @@ public class Util {
 		public IntCoord(int x, int y) {
 			this.x = x;
 			this.y = y;
+		}
+		public boolean sameValue(IntCoord other) {
+			return (this.x == other.x) && (this.y == other.y);
+		}
+		@Override
+		public boolean equals(Object other) {
+			return (other instanceof IntCoord) && (this.sameValue((IntCoord)other));
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.x, this.y);
 		}
 	}
 }
