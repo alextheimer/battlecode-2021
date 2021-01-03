d804d3b87fb62eb15554c2c95dc489232761bf43
Alex Theimer <theimer@mit.edu>
2021-01-03 07:43:55 -0500

    Added documentation to SearchTest.java.

--- test/SearchTest.java
+++ test/SearchTest.java
@@ -33,43 +33,60 @@ class SearchTest {
 
 	// Utils ---------------------------------------------------------
 
-	boolean intCoordInBounds(final IntCoord coord, final int xMin, final int xMax, final int yMin, final int yMax) {
+	/**
+	 * Returns true if coord lies within the space defined by:
+	 *     {coord.x on [xMin, xMax) && coord.y on [yMin, yMax)}
+	 */
+	boolean intCoordInBounds(final IntCoord coord, final int xMin, final int xMax,
+			                 final int yMin, final int yMax) {
 		return ((coord.x >= xMin) && (coord.x < xMax) &&
 			    (coord.y >= yMin) && (coord.y < yMax));
 	}
 
 	// isEndgameCheck ------------------------------------------------
 
+	/**
+	 * Returns a Predicate that returns true if and only if its argument equals() goal.
+	 */
 	Predicate<IntCoord> makeEndgamePred(final IntCoord goal) {
 		return coord -> goal.equals(coord);
 	}
 
 	// cost ----------------------------------------------------------
 
+	/**
+	 * Returns the Euclidian distance between the argument IntCoords.
+	 */
 	double cost(final IntCoord coordA, final IntCoord coordB) {
 		return Math.sqrt(Math.pow(coordA.x - coordB.x, 2) + (Math.pow(coordA.y - coordB.y, 2)));
 	}
 
 	// heuristic -----------------------------------------------------
 
+	/**
+	 * Returns a Function that returns the Euclidian distance of an IntCoord from goal.
+	 */
 	Function<IntCoord, Double> makeHeuristicFunc(final IntCoord goal) {
 		return coord -> cost(coord, goal);
 	}
 
 	// expand --------------------------------------------------------
 
-	Function<IntCoord, Set<IntCoord>> makeExpandFuncCardinal(final IntCoord baseCoord, final int xMin,
-			                                                 final int xMax, final int yMin, final int yMax) {
+	/**
+	 * Returns a Function that expands an IntCoord into its cardinal neighbors. TODO(theimer): explain.
+	 */
+	Function<IntCoord, Set<IntCoord>> makeExpandFuncCardinal(final int xMin, final int xMax,
+			                                                 final int yMin, final int yMax) {
 		return new Function<IntCoord, Set<IntCoord>>() {
 			@Override
 			public Set<IntCoord> apply(final IntCoord coord) {
-				assert intCoordInBounds(baseCoord, xMin, xMax, yMin, yMax);  // TODO(theimer): message
+				assert intCoordInBounds(coord, xMin, xMax, yMin, yMax);  // TODO(theimer): message
 				// TODO(theimer): make this faster if speed matters.
 				return Stream.of(
-					new IntCoord(baseCoord.x + 1, baseCoord.y),
-					new IntCoord(baseCoord.x - 1, baseCoord.y),
-					new IntCoord(baseCoord.x, baseCoord.y + 1),
-					new IntCoord(baseCoord.x, baseCoord.y - 1)
+					new IntCoord(coord.x + 1, coord.y),
+					new IntCoord(coord.x - 1, coord.y),
+					new IntCoord(coord.x, coord.y + 1),
+					new IntCoord(coord.x, coord.y - 1)
 				).filter(expandedCoord -> intCoordInBounds(expandedCoord, xMin, xMax, yMin, yMax))
 			     .collect(Collectors.toSet());
 			}
