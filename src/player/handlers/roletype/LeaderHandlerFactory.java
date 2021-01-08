fa51fdc8626de31259a2d1ff1c61347dcd11b8bc
Alex Theimer <theimer@mit.edu>
2021-01-08 10:41:59 -0500

    Added scanPassabilities to LeaderHandler.

--- src/player/handlers/roletype/LeaderHandlerFactory.java
+++ src/player/handlers/roletype/LeaderHandlerFactory.java
@@ -3,6 +3,15 @@ package player.handlers.roletype;
 import battlecode.common.*;
 import player.handlers.HandlerCommon.IRobotHandler;
 import player.handlers.HandlerCommon.RobotState;
+import util.search.BFSGenerator;
+
+import static util.Util.IntVec2D;
+import util.Util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static player.handlers.HandlerCommon.*;
 
@@ -10,6 +19,28 @@ class LeaderHandler implements IRobotHandler {
 	
 	public LeaderHandler() {
 		// intentionally blank
+	}
+	
+	private static Set<IntVec2D> expandCoord(IntVec2D coord, RobotController rc) {
+		return Util.makeAllAdjacentStream(coord).filter(
+				expCoord ->
+					((expCoord.x != coord.x) ^ (expCoord.y != coord.y)) &&  // only cardinal
+					(rc.canSenseLocation(new MapLocation(coord.x, coord.y)))  // sensable location
+				).collect(Collectors.toSet());
+	}
+	
+	private Map<IntVec2D, Double> scanPassabilities(RobotController rc) {
+		MapLocation currLoc = rc.getLocation();
+		Map<IntVec2D, Double> passabilityMap = new HashMap<>();
+		BFSGenerator<IntVec2D> bfsGen = new BFSGenerator<>(new IntVec2D(currLoc.x, currLoc.y), coord -> expandCoord(coord, rc));
+		for (IntVec2D vec = bfsGen.next(); vec != null; vec = bfsGen.next()) {
+			try {
+				passabilityMap.put(vec, rc.sensePassability(new MapLocation(vec.x, vec.y)));
+			} catch (GameActionException e) {
+				assert false : "expandCoord should prevent this!";
+			}
+		}
+		return passabilityMap;
 	}
 	
 	@Override
--- src/util/Util.java
+++ src/util/Util.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.util.Objects;
+import java.util.stream.Stream;
 
 /**
  * Contains utility functions/classes.
@@ -57,5 +58,28 @@ public class Util {
 		public String toString() {
 			return "(" + this.x + ", " + this.y + ")";
 		}
+	}
+	
+	public static Stream<IntVec2D> makeAllAdjacentStream(IntVec2D coord) {
+		return Stream.of(
+				new IntVec2D(coord.x, coord.y + 1),
+				new IntVec2D(coord.x, coord.y - 1),
+				new IntVec2D(coord.x + 1, coord.y),
+				new IntVec2D(coord.x + 1, coord.y + 1),
+				new IntVec2D(coord.x + 1, coord.y - 1),
+				new IntVec2D(coord.x - 1, coord.y),
+				new IntVec2D(coord.x - 1, coord.y + 1),
+				new IntVec2D(coord.x - 1, coord.y - 1)
+			);
+	}
+	
+	/**
+	 * Returns true if coord lies within the space defined by:
+	 *     {coord.x on [xMin, xMax) && coord.y on [yMin, yMax)}
+	 */
+	public static boolean intCoordInBounds(final IntVec2D coord, final int xMin, final int xMax,
+			                 final int yMin, final int yMax) {
+		return ((coord.x >= xMin) && (coord.x < xMax) &&
+			    (coord.y >= yMin) && (coord.y < yMax));
 	}
 }
