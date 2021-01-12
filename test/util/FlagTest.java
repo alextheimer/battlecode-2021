422efca9ef56c4ecd1b9b994251a93321af97b55
Alex Theimer <theimer@mit.edu>
2021-01-12 11:23:48 -0500

    Added SquadAssignFlag test.

---
+++ test/util/FlagTest.java
@@ -0,0 +1,33 @@
+package util;
+
+import static org.junit.Assert.*;
+import util.Flag.*;
+
+import org.junit.Test;
+
+import player.handlers.HandlerCommon.*;
+
+public class FlagTest {
+	
+	/**
+	 * ~~~ Test Partitions ~~~
+	 * aStar:
+	 *     encode() (all flag types)
+	 *         -TODO
+	 *     decode() (all flag types)
+	 *         -TODO
+	 */
+	
+	@Test
+	public void squadAssign() {
+		SquadType squadType = SquadType.PATROL;
+		int degrees = 67;
+		SquadAssignFlag flag = new SquadAssignFlag(squadType, degrees);
+		int rawFlag = flag.encode();
+		SquadAssignFlag parsedFlag = SquadAssignFlag.decode(rawFlag);
+		assertEquals("rawFlag: " + rawFlag, OpCode.SQUAD_ASSIGN, Flag.getOpCode(rawFlag));
+		assertEquals(degrees, parsedFlag.getOutboundDegrees());
+		assertEquals(squadType, parsedFlag.getSquadType());
+	}
+
+}
