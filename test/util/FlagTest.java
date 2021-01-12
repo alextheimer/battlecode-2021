8a56fc68b6e37680e3a0e1566041683daa7bcae0
Alex Theimer <theimer@mit.edu>
2021-01-12 11:57:48 -0500

    Fixed some documentation in FlagTest.

--- test/util/FlagTest.java
+++ test/util/FlagTest.java
@@ -11,20 +11,21 @@ public class FlagTest {
 	
 	/**
 	 * ~~~ Test Partitions ~~~
-	 * aStar:
-	 *     encode() (all flag types)
-	 *         -TODO
-	 *     decode() (all flag types)
-	 *         -TODO
+	 * encode() (all flag types)
+	 *     -TODO
+	 * decode() (all flag types)
+	 *     -TODO
 	 */
 	
 	@Test
 	public void squadAssign() {
 		SquadType squadType = SquadType.PATROL;
-		int degrees = 67;
+		int degrees = 67;  // Just some random value.
 		SquadAssignFlag flag = new SquadAssignFlag(squadType, degrees);
+		// Make sure getters are working as intended.
 		assertEquals(degrees, flag.getOutboundDegrees());
 		assertEquals(squadType, flag.getSquadType());
+		// Make sure encode / decode are working as intended.
 		int rawFlag = flag.encode();
 		SquadAssignFlag parsedFlag = SquadAssignFlag.decode(rawFlag);
 		assertEquals("rawFlag: " + rawFlag, OpCode.SQUAD_ASSIGN, Flag.getOpCode(rawFlag));
