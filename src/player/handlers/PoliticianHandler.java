d89f4218b1ac288ef9e36f49e005bdbb07409ac7
Alex Theimer <theimer@mit.edu>
2021-01-22 13:01:31 -0500

    Rearranged PoliticianHandler logic.

--- src/player/handlers/PoliticianHandler.java
+++ src/player/handlers/PoliticianHandler.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.Random;
 import java.util.Set;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.function.BiPredicate;
@@ -38,57 +39,78 @@ public class PoliticianHandler implements IRobotTypeHandler {
 	
 	private boolean attemptEmpower(RobotController rc, int radiusSquared) throws GameActionException {
 		HandlerCommon.battlecodeAssert(radiusSquared <= RobotType.POLITICIAN.actionRadiusSquared, "radius squared exceeds action radius", rc);
+		boolean empowerSuccessful;
 		if (rc.canEmpower(radiusSquared)) {
-			System.out.println("successful empower");
 			rc.empower(radiusSquared);
-			return true;
+			empowerSuccessful = true;
 		} else {
-			System.out.println("failed empower");
-			return false;
+			empowerSuccessful = false;
 		}
+		System.out.println("empower attempt: " + empowerSuccessful);
+		return empowerSuccessful;
 	}
-	
+		
 	private boolean attemptEmpowerNearestEnemy(RobotController rc) throws GameActionException {
 		Optional<RobotInfo> enemyInfoOpt = HandlerCommon.findNearestEnemy(rc, rc.senseNearbyRobots());
-		boolean empowerSuccessful;
 		if (enemyInfoOpt.isPresent()) {
 			RobotInfo enemyInfo = enemyInfoOpt.get();
 			int enemyDistSquared = rc.getLocation().distanceSquaredTo(enemyInfo.getLocation());
 			if (enemyDistSquared <= rc.getType().actionRadiusSquared) {
-				empowerSuccessful = attemptEmpower(rc, enemyDistSquared);				
-			} else {
-				empowerSuccessful = false;
+				return attemptEmpower(rc, enemyDistSquared);				
 			}
-		} else {
-			empowerSuccessful = false;
 		}
-		return empowerSuccessful;
+		return false;
 	}
 	
-	private void patrolStep(RobotController rc) throws GameActionException {
-		if (!this.moveHandler.get().step(rc)) {
+	private boolean patrolStep(RobotController rc) throws GameActionException {
+		if (this.moveHandler.get().atEndOfLine()) {
 			this.moveHandler.get().reverse();
-			this.moveHandler.get().step(rc);
 		}
+		return this.moveHandler.get().step(rc);
 	}
 	
-	private void handleUnassigned(RobotController rc) throws GameActionException {
+	private Optional<SquadAssignFlag> findAssignmentFlag(RobotController rc) throws GameActionException {
 		Optional<SimpleImmutableEntry<RobotInfo, Integer>> entryOpt = HandlerCommon.findFirstMatchingFlag(
 				rc,
 				rc.senseNearbyRobots(RobotType.POLITICIAN.sensorRadiusSquared, rc.getTeam()),
 				(robotInfo, rawFlag) -> (Flag.getOpCode(rawFlag) == OpCode.SQUAD_ASSIGN)
 		);
 		if (entryOpt.isPresent()) {
-			int rawFlag = entryOpt.get().getValue();
-			SquadAssignFlag flag = SquadAssignFlag.decode(rawFlag);
-			this.role = Optional.of(flag.getSquadType());
-			DoubleVec2D vec = UtilMath.degreesToVec(flag.getOutboundDegrees());
-			DoubleVec2D origin = new DoubleVec2D(rc.getLocation().x, rc.getLocation().y);
-			Line2D line = UtilMath.Line2D.make(vec, origin);
-			this.moveHandler = Optional.of(new LinearMoverHandler(line, vec));
+			return Optional.of(SquadAssignFlag.decode(entryOpt.get().getValue()));
 		} else {
-			this.attemptEmpowerNearestEnemy(rc);
+			return Optional.empty();
 		}
+	}
+	
+	private void selfInitialize(RobotController rc) {
+		Random rand = new Random();
+		int degrees = rand.nextInt(360);
+		MapLocation mapLoc = rc.getLocation();
+		DoubleVec2D vec = UtilMath.degreesToVec(degrees);
+		DoubleVec2D currCoord = new DoubleVec2D(mapLoc.x, mapLoc.y);
+		this.role = Optional.of(SquadType.PATROL);
+		this.moveHandler = Optional.of(new LinearMoverHandler(Line2D.make(vec, currCoord), vec));
+	}
+	
+	private void flagInitialize(RobotController rc, SquadAssignFlag flag) {
+		this.role = Optional.of(flag.getSquadType());
+		DoubleVec2D vec = UtilMath.degreesToVec(flag.getOutboundDegrees());
+		DoubleVec2D origin = new DoubleVec2D(rc.getLocation().x, rc.getLocation().y);
+		Line2D line = UtilMath.Line2D.make(vec, origin);
+		this.moveHandler = Optional.of(new LinearMoverHandler(line, vec));
+	}
+	
+	private void handleUnassigned(RobotController rc) throws GameActionException {
+		Optional<SquadAssignFlag> flagOpt = this.findAssignmentFlag(rc);
+		if (flagOpt.isPresent()) {
+			this.flagInitialize(rc, flagOpt.get());
+			System.out.println("initialized via flag");
+		} else {
+			// not spawned by empowerment center
+			this.selfInitialize(rc);
+			System.out.println("initialized via self");
+		}
+		this.attemptEmpowerNearestEnemy(rc);
 	}
 	
 	private void handlePatrol(RobotController rc) throws GameActionException {
