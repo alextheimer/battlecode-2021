f6a2962aa0346e1daf20950906237672f6d6f311
Alex Theimer <theimer@mit.edu>
2021-02-02 13:06:45 -0500

    Removed useless code/comments from MuckrakerHandler.java

--- src/player/handlers/robots/MuckrakerHandler.java
+++ src/player/handlers/robots/MuckrakerHandler.java
@@ -9,28 +9,9 @@ import player.handlers.common.HandlerCommon;
 
 public class MuckrakerHandler implements RobotPlayer.IRobotHandler {
 	
-	public MuckrakerHandler() {
-		//blank
-	}
-	
 	@Override
 	public RobotPlayer.IRobotHandler handle(RobotController rc) throws GameActionException {
-//        Team enemy = rc.getTeam().opponent();
-//        int actionRadius = rc.getType().actionRadiusSquared;
-//        for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
-//            if (robot.type.canBeExposed()) {
-//                // It's a slanderer... go get them!
-//                if (rc.canExpose(robot.location)) {
-//                    UtilBattlecode.log("e x p o s e d");
-//                    rc.expose(robot.location);
-//                    return this;
-//                }
-//            }
-//        }
-//        if(attemptMove(rc, randomDirection()))
-//            UtilBattlecode.log("I moved!");
+		// intentionally blank (unit unused)
         return this;
 	}
-
-
 }
