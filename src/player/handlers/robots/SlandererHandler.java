4eac215ef235b4c3b13872ff740f06349325b45b
Alex Theimer <theimer@mit.edu>
2021-02-03 17:12:32 -0500

    Documentation/refactor in SlandererHandler.java.

--- src/player/handlers/robots/SlandererHandler.java
+++ src/player/handlers/robots/SlandererHandler.java
@@ -4,8 +4,10 @@ import battlecode.common.*;
 import player.RobotPlayer;
 import player.RobotPlayer.IRobotHandler;
 import player.handlers.common.HandlerCommon;
+import player.handlers.common.PredicateFactories;
 import player.util.battlecode.UtilBattlecode;
 import player.util.battlecode.flag.Flag;
+import player.util.battlecode.flag.Flag.IFlag;
 import player.util.battlecode.flag.types.EnemySightedFlag;
 import player.util.battlecode.flag.types.PatrolAssignmentFlag;
 import player.util.battlecode.flag.util.UtilFlag.FlagOpCode;
@@ -14,57 +16,76 @@ import player.util.math.IntVec2D;
 import player.util.math.UtilMath;
 
 import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static player.handlers.common.HandlerCommon.*;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class SlandererHandler implements RobotPlayer.IRobotHandler {
 	
-	private MapLocation origin = null;  // TODO(theimer): should be in constructor / non-nullable
+	// the MapLocation of the Enlightenment Center from which the Slanderer originates.
+	private MapLocation originEnlightenmentCenterMapLoc;
 	
-	public SlandererHandler() {
+	/**
+	 * Handles a Slanderer robot.
+	 * 
+	 * @param rc the RobotController for the current round.
+	 */
+	public SlandererHandler(RobotController rc) throws GameActionException {
+		List<RobotInfo> sensedRobots = Arrays.asList(rc.senseNearbyRobots());
+		// look for an assignment flag
+		Optional<SimpleImmutableEntry<RobotInfo, IFlag>> assignmentEntry = 
+				HandlerCommon.getAnyAdjacentAssignmentFlag(rc, sensedRobots);
+		
+		assert assignmentEntry.isPresent() : "no assignment flag detected!";
+		
+		// store the origin location
+		RobotInfo originEnlightenmentCenter = assignmentEntry.get().getKey();
+		this.originEnlightenmentCenterMapLoc = originEnlightenmentCenter.getLocation();
+		
+		// TODO(theimer): handle the assignment type
 	}
 	
 	@Override
 	public RobotPlayer.IRobotHandler handle(RobotController rc) throws GameActionException {
-		if (rc.getType() != RobotType.SLANDERER) {
-			return new PoliticianHandler();
-		}
 		
-		if (this.origin == null) {
-			this.origin = rc.getLocation();
-		}
+		List<RobotInfo> sensedRobots = Arrays.asList(rc.senseNearbyRobots());
 		
-        Optional<RobotInfo> sensedEnlightenmentCenterOpt = HandlerCommon.getRobotsNotOnTeamStream(rc.getTeam(), rc.senseNearbyRobots())
-        		.filter(robotInfo -> robotInfo.getType() == RobotType.ENLIGHTENMENT_CENTER).findAny();
-        if(sensedEnlightenmentCenterOpt.isPresent()) {
-        	RobotInfo enlightenmentCenterInfo = sensedEnlightenmentCenterOpt.get();
-        	EnemySightedFlag flag = new EnemySightedFlag(RobotType.ENLIGHTENMENT_CENTER, enlightenmentCenterInfo.getLocation());
-        	rc.setFlag(Flag.encode(flag));
+		// call out the highest priority enemy to any listening enlightenment centers
+		HandlerCommon.setHighestPriorityEnemySightedFlag(rc, sensedRobots);
+        
+		// find all non-teammates
+		List<RobotInfo> nonTeamRobots = new ArrayList<>();
+        {
+        	Stream<RobotInfo> nonTeamRobotStream = 
+        			sensedRobots.stream()
+        			.filter(PredicateFactories.robotNonTeam(rc.getTeam()));
+        	UtilGeneral.legalCollect(nonTeamRobotStream, nonTeamRobots);
         }
         
-        Optional<RobotInfo> nearestOpt = HandlerCommon.getNearestNonTeamRobot(rc, rc.senseNearbyRobots());
-        if (nearestOpt.isPresent()) {
-        	MapLocation enemyLoc = nearestOpt.get().getLocation();
-        	Iterator<MapLocation> mapLocIter = UtilBattlecode.makeAdjacentMapLocIterator(rc.getLocation());
-        	Iterator<MapLocation> filteredMapLocIter = UtilGeneral.streamifyIterator(mapLocIter)
-        			.filter(UtilBattlecode.silenceGameActionPredicate(mapLoc -> rc.onTheMap(mapLoc) && !rc.isLocationOccupied(mapLoc))).iterator();
-        	if (filteredMapLocIter.hasNext()) {
-        		MapLocation moveToLoc = UtilGeneral.findLeastCostLinear(filteredMapLocIter, mapLoc -> (double)-mapLoc.distanceSquaredTo(enemyLoc));
-        		HandlerCommon.attemptMove(rc, rc.getLocation().directionTo(moveToLoc));        		
-        	}
+        // we'll always try to move away from some location
+        MapLocation moveAwayFrom;
+        if (nonTeamRobots.size() > 0) {
+        	// move away from an enemy (this is a non-combatant type)
+        	RobotInfo nearestNonTeamRobot = HandlerCommon.getNearestRobot(rc, nonTeamRobots);
+        	moveAwayFrom = nearestNonTeamRobot.getLocation();
         } else {
-        	Iterator<MapLocation> mapLocIter = UtilBattlecode.makeAdjacentMapLocIterator(rc.getLocation());
-        	Iterator<MapLocation> filteredMapLocIter = UtilGeneral.streamifyIterator(mapLocIter)
-        			.filter(UtilBattlecode.silenceGameActionPredicate(mapLoc -> rc.onTheMap(mapLoc) && !rc.isLocationOccupied(mapLoc))).iterator();
-        	if (filteredMapLocIter.hasNext()) {
-        		
-        		MapLocation moveToLoc = UtilGeneral.findLeastCostLinear(filteredMapLocIter, mapLoc -> (double)-mapLoc.distanceSquaredTo(this.origin));
-        		HandlerCommon.attemptMove(rc, rc.getLocation().directionTo(moveToLoc));        		
-        	}
+        	// move away from the enlightenment center (to create space for building)
+        	moveAwayFrom = this.originEnlightenmentCenterMapLoc;
         }
+        
+        // attempt to move as far from moveAwayFrom as possible
+        Function<MapLocation, Double> distanceCostFunc = mapLoc -> (double)-mapLoc.distanceSquaredTo(moveAwayFrom);  // note: negated!
+    	Optional<Direction> awayDirectionOpt = HandlerCommon.getLeastCostMoveDirection(rc, distanceCostFunc);
+    	if (awayDirectionOpt.isPresent()) {
+    		HandlerCommon.attemptMove(rc, awayDirectionOpt.get());        		
+    	} // else no adjacent/on-the-map/unoccupied spaces exist
         
         return this;
 	}
