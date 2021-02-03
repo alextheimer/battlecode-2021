0af4796badb5ce3807f9f5c2c6ca56a24dcb4475
Alex Theimer <theimer@mit.edu>
2021-02-03 15:31:56 -0500

    Lots of refactor/documentation changes in HandlerCommon.java.

--- src/player/handlers/common/HandlerCommon.java
+++ src/player/handlers/common/HandlerCommon.java
@@ -16,12 +16,15 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import com.sun.tools.corba.se.idl.toJavaPortable.Util;
+
 import battlecode.common.*;
 import player.handlers.common.HandlerCommon;
 import player.util.battlecode.UtilBattlecode;
 import player.util.battlecode.flag.Flag;
 import player.util.battlecode.flag.Flag.IFlag;
 import player.util.battlecode.flag.types.AttackAssignmentFlag;
+import player.util.battlecode.flag.types.EnemySightedFlag;
 import player.util.battlecode.flag.types.PatrolAssignmentFlag;
 import player.util.battlecode.flag.util.UtilFlag.FlagOpCode;
 import player.util.general.UtilGeneral;
@@ -64,23 +67,19 @@ public class HandlerCommon {
 	}
 	
 	/**
-	 * Returns a Stream of RobotInfos on the specified Team.
+	 * Sets the robot's flag.
 	 * 
-	 * @param sensedRobots RobotInfos sensed by the RobotController for the current round.
+	 * @param rc the RobotController for the current round.
 	 */
-	public static Stream<RobotInfo> getRobotsOnTeamStream(Team team, RobotInfo[] sensedRobots) {
-		return Arrays.stream(sensedRobots)
-				.filter(robotInfo -> robotInfo.getTeam() == team);
-	}
-	
-	/**
-	 * Returns a Stream of RobotInfos ***NOT*** on the specified Team.
-	 * 
-	 * @param sensedRobots RobotInfos sensed by the RobotController for the current round.
-	 */
-	public static Stream<RobotInfo> getRobotsNotOnTeamStream(Team notThisTeam, RobotInfo[] sensedRobots) {
-		return Arrays.stream(sensedRobots)
-				.filter(robotInfo -> robotInfo.getTeam() != notThisTeam);
+	public static void setFlag(RobotController rc, IFlag flag) {
+		int rawFlag = Flag.encode(flag);
+		assert rc.canSetFlag(rawFlag);
+		try {
+			rc.setFlag(rawFlag);			
+		} catch (GameActionException e) {
+			// rc.canSetFlag is a precondition, so this shouldn't happen!
+			throw new UtilBattlecode.IllegalGameActionException(e);
+		}
 	}
 	
 	/**
@@ -88,11 +87,15 @@ public class HandlerCommon {
 	 * robot is on the same Team as the RobotController robot.
 	 * 
 	 * @param rc the RobotController for the current round.
-	 * @param sensedRobots RobotInfos sensed by the RobotController for the current round.
+	 * @param robotCollection the robots to be included in the robot/flag pairs.
+	 * 	   Must have been sensed by the controller during this round (i.e. the RobotInfos are up-to-date).
+	 *     Note that all non-team robots will be automatically filtered from the result.
 	 */
-	public static Stream<SimpleImmutableEntry<RobotInfo, IFlag>> getMatchingTeamFlagsStream(RobotController rc, 
-			                                                                                RobotInfo[] sensedRobots) {
-		return getRobotsOnTeamStream(rc.getTeam(), sensedRobots)
+	public static Stream<SimpleImmutableEntry<RobotInfo, IFlag>> getTeamFlagStream(RobotController rc, 
+			                                                                       Collection<RobotInfo> robotCollection) {
+		return robotCollection.stream()
+			// get only the robots on our team
+			.filter(PredicateFactories.robotSameTeam(rc.getTeam()))
 		    // map to RobotInfo/IFlag pairs
 		    .map(new Function<RobotInfo, SimpleImmutableEntry<RobotInfo, IFlag>>() {
 
@@ -110,27 +113,20 @@ public class HandlerCommon {
 			});
 	}
 	
-	
 	/**
-	 * Returns the robot nearest to the RobotController robot that is ***NOT*** on the same team.
+	 * Returns the robot nearest to the RobotController robot.
 	 * 
 	 * @param rc the RobotController for the current round.
-	 * @param sensedRobots RobotInfos sensed by the RobotController for the current round.
-	 * @return an occupied Optional iff sensedRobots contains at least one non-team RobotInfo;
-	 *     else returns an empty Optional.
+	 * @param robotCollection the RobotInfos sensed by the RobotController for the current round.
+	 *     Must have been sensed by the controller during this round (i.e. the RobotInfos are up-to-date).
+	 *     Must have size > 0.
 	 */
-	public static Optional<RobotInfo> getNearestNonTeamRobot(RobotController rc, RobotInfo[] sensedRobots) {
-		Iterator<RobotInfo> nonTeamRobotIterator = getRobotsNotOnTeamStream(rc.getTeam(), sensedRobots).iterator();
-		Optional<RobotInfo> result;
-		if (nonTeamRobotIterator.hasNext()) {
-			Function<RobotInfo, Double> costFunc = robotInfo -> (double)rc.getLocation().distanceSquaredTo(robotInfo.getLocation());
-			result = Optional.of(UtilGeneral.findLeastCostLinear(nonTeamRobotIterator, costFunc));
-		} else {
-			result = Optional.empty();
-		}
-		return result;
+	public static RobotInfo getNearestRobot(RobotController rc, Collection<RobotInfo> robotCollection) {
+		assert robotCollection.size() > 0 :
+			"robotCollection must have positive size; size: " + robotCollection.size();
+		Function<RobotInfo, Double> costFunc = robotInfo -> (double)rc.getLocation().distanceSquaredTo(robotInfo.getLocation());
+		return UtilGeneral.getLeastCostLinear(robotCollection, costFunc);
 	}
-	
 	
 	/**
 	 * Returns some robot/flag pair such that:
@@ -139,49 +135,106 @@ public class HandlerCommon {
 	 *     (3) the flag is an "assignment" flag.  // TODO(theimer): make this definition more clear/concrete
 	 * 
 	 * @param rc the RobotController for the current round.
-	 * @param sensedRobots RobotInfos sensed by the RobotController for the current round.
-	 * @return an occupied Optional iff a RobotInfo in `sensedRobots` has posted a flag, and the
+	 * @param robotCollection RobotInfos sensed by the RobotController during the current round.
+	 *     Note that all non-team robots are automatically filtered.
+	 * @return an occupied Optional iff a RobotInfo in the collection has posted a flag, and the
 	 *     RobotInfo/IFlag pair meets the above three criteria.
 	 */
 	public static Optional<SimpleImmutableEntry<RobotInfo, IFlag>> getAnyAdjacentAssignmentFlag(RobotController rc,
-			                                                                                    RobotInfo[] sensedRobots) {
-		// max distance squared between two adjacent MapLocations
-		final int maxAdjacentDistanceSquared = 2;
-		
-		BiPredicate<RobotInfo, IFlag> predicate = new BiPredicate<RobotInfo, IFlag>() {
-
-			@Override
-			public boolean test(RobotInfo robotInfo, IFlag flag) {
-				return
-						// adjacent to the RobotController robot
-						(rc.getLocation().distanceSquaredTo(robotInfo.getLocation()) <= maxAdjacentDistanceSquared) &&
-						// is an assignment flag
-						(flag instanceof PatrolAssignmentFlag || flag instanceof AttackAssignmentFlag);
-			}
-			
-		};
-		
-		return getMatchingTeamFlagsStream(rc, sensedRobots)
-				.filter(entry -> predicate.test(entry.getKey(), entry.getValue()))
+			                                                                                    Collection<RobotInfo> robotCollection) {
+		return 
+				// get only flags for the robots on our team
+				getTeamFlagStream(rc, robotCollection)
+				// filter out non-adjacent robots
+				.filter(entry -> PredicateFactories.robotAdjacentTo(rc.getLocation()).test(entry.getKey()))
+				// filter out non-assignment flags
+				.filter(entry -> PredicateFactories.flagAssignment().test(entry.getValue()))
 				.findAny();
 	}
 	
-	
 	/**
-	 * Returns a Stream of all locations that the RobotController can move into such that each:
-	 *     (1) is adjacent to the robot's current location, and
-	 *     (2) is on the map.
+	 * Performs three actions:
+	 * 
+	 * (1) Finds the non-teammate robots in a collection.
+	 * (2) Assigns each a 'priority'.
+	 * (3) Sets an EnemySightedFlag to signal the non-teammate robot of highest priority.
+	 * 
+	 * If no non-teammate robot is found, (2) and (3) do not execute.
 	 * 
 	 * @param rc the RobotController for the current round.
+	 * @param robotCollection the collection of robots to search through.
+	 *     Must have been sensed during the current round (i.e. the RobotInfos are up-to-date).
+	 *     Note that teammate robots are automatically ignored.
+	 * @return true iff a flag is set; else false.
 	 */
-	public static Stream<MapLocation> makeValidAdjacentMapLocStream(RobotController rc) {
-		MapLocation currentMapLoc = rc.getLocation();
+	public static boolean setHighestPriorityEnemySightedFlag(RobotController rc, Collection<RobotInfo> robotCollection) {
 		
-		Stream<MapLocation> adjacentMapLocStream = UtilBattlecode.makeAllAdjacentMapLocStream(rc.getLocation());
+		/* The three steps of this function occur verbatim in all non-Enlightenment-Center handlers. */
 		
-		// Note: GameActionException will not be thrown; adjacent MapLocations are always in sensor range.
-		Predicate<MapLocation> onTheMapPredicate = UtilBattlecode.<MapLocation>silenceGameActionPredicate(mapLoc -> rc.onTheMap(mapLoc));
+		// TODO(theimer): improve this cost Function; make unique to the controller?
+		Function<RobotInfo, Double> costFunc = new Function<RobotInfo, Double>(){
+			@Override
+			public Double apply(RobotInfo robotInfo) {
+				switch(robotInfo.getType()) {
+					// costs are negated since we use a least-cost utility function.
+					case POLITICIAN: return -1.0;
+					case MUCKRAKER: return -2.0;
+					case SLANDERER: return -3.0;
+					case ENLIGHTENMENT_CENTER: return -4.0;
+					default: throw new RuntimeException("unrecognized RobotInfo: " + robotInfo.getType());
+				}
+			}
+		};
 		
-		return adjacentMapLocStream.filter(onTheMapPredicate);
+		// arbitrarily assume the flag is not set.
+		boolean flagSet = false;
+		
+		// get all non-teammate robots
+		Stream<RobotInfo> nonTeamRobotStream = robotCollection.stream().filter(PredicateFactories.robotNonTeam(rc.getTeam()));
+		List<RobotInfo> nonTeamRobotList = new ArrayList<>();
+		UtilGeneral.legalCollect(nonTeamRobotStream, nonTeamRobotList);
+		
+		if (nonTeamRobotList.size() > 0) {
+			// find the one of highest priority
+			RobotInfo highestPriorityRobot = UtilGeneral.getLeastCostLinear(nonTeamRobotList, costFunc);
+			// post the flag!
+			EnemySightedFlag flag = new EnemySightedFlag(highestPriorityRobot.getType(), highestPriorityRobot.getLocation());
+			HandlerCommon.setFlag(rc, flag);
+			flagSet = true;
+		}
+		
+		return flagSet;
+	}
+	
+	/**
+	 * Returns the Direction a robot should move in order to occupy the adjacent,
+	 * least-cost MapLocation that is:
+	 *     (1) on the map, and
+	 *     (2) unoccupied.
+	 * @param rc the RobotController for the current round.
+	 * @param costFunc a function that maps all MapLocations to a cost value.
+	 * @return an occupied Optional iff at least one adjacent MapLocation satisfies
+	 *     the above two constraints; else an empty Optional.
+	 */
+	public static Optional<Direction> getLeastCostMoveDirection(RobotController rc, Function<MapLocation, Double> costFunc) {
+		// get all adjacent/on-the-map/unoccupied MapLocations.
+		Stream<MapLocation> validAdjacentStream =
+				UtilBattlecode.makeAllAdjacentMapLocStream(rc.getLocation())
+				.filter(PredicateFactories.mapLocOnMap(rc))
+				.filter(PredicateFactories.mapLocUnoccupied(rc));
+		
+		// collect the stream into a list
+		List<MapLocation> validAdjacentList = new ArrayList<>();
+		UtilGeneral.legalCollect(validAdjacentStream, validAdjacentList);
+		
+		Optional<Direction> result;
+		if (validAdjacentList.size() > 0) {
+			// convert the least-cost MapLoc to a Direction
+			MapLocation leastCostMapLoc = UtilGeneral.getLeastCostLinear(validAdjacentList, costFunc);
+			result = Optional.of(rc.getLocation().directionTo(leastCostMapLoc));
+		} else {
+			result = Optional.empty();
+		}
+		return result;
 	}
 }
