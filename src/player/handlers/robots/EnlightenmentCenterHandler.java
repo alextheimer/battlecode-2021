1c8d2338c05b4f71ac2e74cae0d7de88542239d5
Alex Theimer <theimer@mit.edu>
2021-02-05 12:59:36 -0500

    Documentation/refactor in EnlightenmentCenterHandler.java.

--- src/player/handlers/robots/EnlightenmentCenterHandler.java
+++ src/player/handlers/robots/EnlightenmentCenterHandler.java
@@ -4,6 +4,7 @@ import battlecode.common.*;
 import player.RobotPlayer;
 import player.RobotPlayer.IRobotHandler;
 import player.handlers.common.HandlerCommon;
+import player.handlers.common.PredicateFactories;
 import player.util.battlecode.UtilBattlecode;
 import player.util.battlecode.flag.Flag;
 import player.util.battlecode.flag.Flag.IFlag;
@@ -21,7 +22,9 @@ import static player.handlers.common.HandlerCommon.*;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -33,75 +36,150 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 class Target implements Comparable<Target> {
 	
-	RobotType robotType;
-	MapLocation mapLoc;
+	public final RobotType robotType;
+	public final MapLocation mapLoc;
 	
+	/**
+	 * Immutable struct-like storage for an Enlightenment Center target.
+	 */
 	public Target(RobotType robotType, MapLocation mapLoc) {
 		this.robotType = robotType;
 		this.mapLoc = mapLoc;
 	}
 	
 	@Override
-	public int hashCode() {
-		return Objects.hash(robotType, mapLoc);
-	}
-	
-	public boolean sameValue(Target other) {
-		return (this.robotType == other.robotType) && (this.mapLoc.equals(other.mapLoc));
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		return (other instanceof Target) && this.sameValue((Target)other);
-	}
-	
-	@Override
 	public int compareTo(Target other) {
-		return 0;
+		// Enlightenment Centers are sorted before all other types.
+		if (this.robotType == other.robotType) {
+			return 0;
+		} else if (this.robotType == RobotType.ENLIGHTENMENT_CENTER){
+			return -1;
+		} else if (other.robotType == RobotType.ENLIGHTENMENT_CENTER) {
+			return 1;
+		} else {
+			return 0;
+		}
 	}
-
+	
 }
 
-class Blueprint {
-	public RobotType robotType;
-	public EnlightenmentCenterHandler.Assignment assignmentType;
-	public Blueprint(RobotType robotType, EnlightenmentCenterHandler.Assignment assignmentType) {
-		this.robotType = robotType;
-		this.assignmentType = assignmentType;
-	}
-}
-
-public class EnlightenmentCenterHandler implements RobotPlayer.IRobotHandler {
+/**
+ * Priority queue for Targets, where each pushed Target "claims" its MapLocation.
+ * No two Targets can claim the same MapLocation.
+ */
+class TargetQueue {
+	
+	private Queue<Target> pQueue = new PriorityQueue<>();
+	private Map<MapLocation, Target> mapLocMap = new HashMap<>();  // supports remove()
 	
 	/**
-	 * Each unit is given an Assignment 
+	 * Claims the Target's MapLocation, then pushes it onto the queue.
+	 * 
+	 * @param target must specify an unclaimed MapLocation.
 	 */
-	public static enum Assignment { PATROL, ATTACK_TARGET, UNASSIGNED }
+	public void push(Target target) {
+		assert !this.mapLocMap.containsKey(target.mapLoc) : "MapLocation already claimed: " + target.mapLoc;
+		this.pQueue.add(target);
+		this.mapLocMap.put(target.mapLoc, target);
+	}
 	
+	/**
+	 * Returns the Target at the head of the queue.
+	 * Queue must be non-emepty.
+	 */
+	public Target peek() {
+		assert this.pQueue.size() > 0 : "cannot peek on empty queue";
+		return this.pQueue.peek();
+	}
+	
+	/**
+	 * Pops and returns the Target from the head of the queue.
+	 * Unclaims the Target's MapLocation.
+	 */
+	public Target pop() {
+		assert this.pQueue.size() > 0 : "cannot pop from an empty queue";
+		Target popped = this.pQueue.remove();
+		this.mapLocMap.remove(popped.mapLoc);
+		return popped;
+	}
+	
+	/**
+	 * Returns true iff the MapLocation is claimed.
+	 */
+	public boolean mapLocClaimed(MapLocation mapLoc) {
+		return this.mapLocMap.containsKey(mapLoc);
+	}
+	
+	/**
+	 * Unclaims the MapLocation and returns its associated Target.
+	 * @param mapLoc must be claimed.
+	 */
+	public Target remove(MapLocation mapLoc) {
+		assert this.mapLocMap.containsKey(mapLoc) : "unclaimed MapLocation: " + mapLoc;
+		Target removed = this.mapLocMap.get(mapLoc);
+		this.mapLocMap.remove(mapLoc);
+		this.pQueue.remove(removed);
+		return removed;
+	}
+	
+	/**
+	 * Returns the size of the queue.
+	 */
+	public int size() {
+		return this.pQueue.size();
+	}
+	
+}
+
+/**
+ * Handles Enlightenment Center RobotControllers.
+ */
+public class EnlightenmentCenterHandler implements RobotPlayer.IRobotHandler {
+	
+	// number of rounds to TODO
 	private static final int FLAG_COOLDOWN_START = 1;
-	private static final int DEGREES_DELTA = 5;
-	private static final int DEGREES_START = 0;
 	
+	// >0 indicates a flag needs to remain posted
 	private int flagCooldown = 0;
-	private Queue<Target> targetQueue = new PriorityQueue<>();
-	private Set<Target> addedTargets = new HashSet<>();
-	private int nextDegrees = DEGREES_START;
-	private Set<Integer> idSet = new HashSet<>();
-	private int buildNum = 0;
+	private TargetQueue targetQueue = new TargetQueue();
+	// stores all currently-known teammate Id's
+	private Set<Integer> friendlyIdSet = new HashSet<>();
+	private int numUnitsBuilt = 0;
 	
-    private boolean attemptBuild(RobotController rc, Blueprint blueprint, Flag.IFlag assignmentFlag) throws GameActionException {
-    	final int influence = rc.getInfluence() - 1;
+	/**
+	 * Attempts to build a robot with all remaining influence and post its assignment flag.
+	 * The operation fails iff:
+	 *     (1) a positive flagCooldown exists, or
+	 *     (2) the unit is not buildable in any adjacent space.
+	 *     
+	 * @param rc the RobotController for the current round.
+	 * @param robotType the RobotType to build.
+	 * @param assignmentFlag the assignment flag to post. // TODO(theimer): add a set of assignment types in Flag.java.
+	 * @return true iff the operation succeeds.
+	 */
+    private boolean attemptBuild(RobotController rc, RobotType robotType, Flag.IFlag assignmentFlag) {
+    	assert this.flagCooldown == 0 : "flagCooldown != 0 : " + this.flagCooldown;
+    	final int influence = rc.getInfluence() - 1;  // -1 so we don't use all the influence and die
+    	// arbitrarily assume the build fails
     	boolean buildSuccess = false;
+    	// attempt to build in every direction
     	for (Direction dir : UtilBattlecode.OFF_CENTER_DIRECTIONS) {
-    		if (rc.canBuildRobot(blueprint.robotType, dir, influence)) {
-    			rc.buildRobot(blueprint.robotType, dir, influence);
-    			rc.setFlag(Flag.encode(assignmentFlag));
+    		if (rc.canBuildRobot(robotType, dir, influence)) {
+    			try {
+    				rc.buildRobot(robotType, dir, influence);    				
+    			} catch (GameActionException e) {
+    				// should never happen; canBuildRobot is a precondition
+    				throw new UtilBattlecode.IllegalGameActionException(e);
+    			}
+    			HandlerCommon.setFlag(rc, assignmentFlag);
     			this.flagCooldown = FLAG_COOLDOWN_START;
     			buildSuccess = true;
-    			buildNum++;
+    			this.numUnitsBuilt++;
     			break;
     		}
     	}
@@ -109,103 +187,152 @@ public class EnlightenmentCenterHandler implements RobotPlayer.IRobotHandler {
     	return buildSuccess;
     }
     
-    private Blueprint makeBlueprint(Target target) {
-    	return new Blueprint(RobotType.POLITICIAN, Assignment.ATTACK_TARGET);
-    }
-    
-    private boolean targetFilter(Target target, RobotController rc) {
-    	return (target.robotType == RobotType.ENLIGHTENMENT_CENTER) && !rc.getLocation().equals(target.mapLoc) && !this.addedTargets.contains(target);
-    }
-    
-    private int incrementDegrees() {
-    	int nextDegrees = this.nextDegrees;
-    	this.nextDegrees += 90;
-    	if (this.nextDegrees >= UtilMath.CIRCLE_DEGREES) {
-    		this.nextDegrees -= UtilMath.CIRCLE_DEGREES;
-    		this.nextDegrees += DEGREES_DELTA;
+    /**
+     * Stores senseable teammate ID's and removes unsenseable ones.
+     * 
+     * @param sensedRobots a Collection of robots sensed during the current round.
+     *     Note: filters are applied automatically in this function; the caller
+     *     doesn't necessarily need to apply any themselves.
+     * @param rc the RobotController for the current round.
+     */
+    private void updateKnownFriendlyIds(Collection<RobotInfo> sensedRobots, RobotController rc) {
+    	// add all new ids
+    	sensedRobots.stream()
+    		.filter(PredicateFactories.robotSameTeam(rc.getTeam()))
+    		.map(robotInfo -> robotInfo.getID())
+    		// make sure we don't already know about the id
+    		.filter(robotId -> !this.friendlyIdSet.contains(robotId))
+    		.forEach(robotId -> this.friendlyIdSet.add(robotId));
+    	
+    	// remove unsenseable (i.e. dead/converted) ids (E-Centers can sense robots at any distance)
+    	Iterator<Integer> idIterator = this.friendlyIdSet.iterator();
+    	while (idIterator.hasNext()) {
+    		int id = idIterator.next();
+    		if (!rc.canSenseRobot(id)) {
+    			idIterator.remove();
+    		}
     	}
-    	return nextDegrees;
+    }
+    
+    /**
+     * Updates the target queue to reflect any new info in the EnemySightedFlag.
+     * 
+     * @param rc the RobotController for the current round.
+     */
+    private void readEnemySightedFlag(RobotController rc, EnemySightedFlag flag) {
+    	MapLocation mapLoc = flag.getMapLoc(rc.getLocation());
+    	Target target = new Target(flag.getRobotType(), mapLoc);
+    	// is the MapLocation already associated with a target?
+    	if (this.targetQueue.mapLocClaimed(mapLoc)) {
+    		// assume the latest flag is more up-to-date and remove the old target
+    		this.targetQueue.remove(mapLoc);
+    	}
+    	this.targetQueue.push(target);
+    }
+    
+    /**
+     * Updates the target queue to reflect any new info in the TargetMissingFlag.
+     * 
+     * @param rc the RobotController for the current round.
+     */
+    private void readTargetMissingFlag(RobotController rc, TargetMissingFlag flag) {
+    	MapLocation mapLoc = flag.getMapLoc(rc.getLocation());
+    	// remove any targets we thought existed there
+    	if (this.targetQueue.mapLocClaimed(mapLoc)) {
+    		this.targetQueue.remove(mapLoc);
+    	}
+    }
+    
+    /**
+     * Listens for any flags that might affect the Target queue.
+     * Update the queue to reflect new information.
+     * 
+     * @param rc the RobotController for the current round.
+     */
+    private void updateTargetQueue(RobotController rc) {
+    	Function<Integer, IFlag> flagMapper = new Function<Integer, IFlag>() {
+			@Override
+			public IFlag apply(Integer id) {
+				assert rc.canSenseRobot(id);
+				try {
+					return Flag.decode(rc.getFlag(id));
+				} catch (GameActionException e) {
+					// shouldn't happen-- canSenseRobot is a precondition.
+					throw new UtilBattlecode.IllegalGameActionException(e);
+				}
+			}
+    	};
+    	
+    	Stream<IFlag> flagStream = this.friendlyIdSet.stream().map(flagMapper);
+    	
+    	// get/read all non-teammate callout flags
+    	flagStream.filter(flag -> flag instanceof EnemySightedFlag)
+    		.forEach(flag -> this.readEnemySightedFlag(rc, (EnemySightedFlag)flag));
+    	
+    	// get/read all "target isn't where it's expected" flags
+    	flagStream.filter(flag -> flag instanceof TargetMissingFlag)
+    		.forEach(flag -> this.readTargetMissingFlag(rc, (TargetMissingFlag)flag));
+    		
+    }
+    
+    /**
+     * Plans a unit/assignment-flag pair to build/post.
+     * Attempts to build the unit and post the flag.
+     * 
+     * @param rc the RobotController for the current round.
+     * @return true iff the unit is successfully built and its flag is posted.
+     */
+    private boolean attemptNextBuild(RobotController rc) {
+    	boolean buildSuccess;
+    	// Oscillate between Slanderers and Politicians
+		if (this.numUnitsBuilt % 2 == 1) {
+			// assign the slanderer to patrol in a random direction
+			Random rand = new Random();
+			int patrolDegrees = rand.nextInt(UtilMath.CIRCLE_DEGREES);
+			buildSuccess = this.attemptBuild(rc, RobotType.SLANDERER, new PatrolAssignmentFlag(patrolDegrees));
+		} else if (this.targetQueue.size() > 0) {
+			// we have a target to attack
+			Target target = this.targetQueue.peek();
+			AttackAssignmentFlag flag = new AttackAssignmentFlag(target.mapLoc);
+			buildSuccess = this.attemptBuild(rc, RobotType.POLITICIAN, flag);
+			if (buildSuccess) {
+				this.targetQueue.pop();
+			}
+		} else {
+			// no targets to attack; just patrol in a random direction
+			Random rand = new Random();
+			int patrolDegrees = rand.nextInt(UtilMath.CIRCLE_DEGREES);
+			buildSuccess = this.attemptBuild(rc, RobotType.POLITICIAN, new PatrolAssignmentFlag(patrolDegrees));
+		}
+		return buildSuccess;
     }
     
 	@Override
 	public RobotPlayer.IRobotHandler handle(RobotController rc) throws GameActionException {
-
-		if (this.flagCooldown > 0) {
-			// just built something; still need to keep its assignment flag up
-			this.flagCooldown--;
-			return this;
-		}
 		
 		// store this; use below
-		RobotInfo[] sensedRobots = rc.senseNearbyRobots();
+		List<RobotInfo> sensedRobots = Arrays.asList(rc.senseNearbyRobots());
 		
 		// update our known ID's
-		Set<RobotInfo> teamSet = UtilGeneral.legalSetCollect(HandlerCommon.getRobotsOnTeamStream(rc.getTeam(), sensedRobots));
-		for (RobotInfo robotInfo : teamSet) {
-			if (!this.idSet.contains(robotInfo.getID())) {
-				this.idSet.add(robotInfo.getID());
-			}
-		}
+		this.updateKnownFriendlyIds(sensedRobots, rc);
 		
 		// check if we need to call for help
-		Optional<RobotInfo> nearestNonTeam = HandlerCommon.getNearestNonTeamRobot(rc, sensedRobots);
-		if (nearestNonTeam.isPresent()) {
-			// TODO(theimer): call for help!
-		}
+		// TODO(theimer): make this happen
 		
-		// look for EC's in need of help; build and deploy if necessary
-		// TODO(theimer): !!!
+		// look for EC's in need of help; build and deploy if deemed appropriate
+		// TODO(theimer): make this happen
 		
-		// look for enemy callouts; screen; store targets
-		Iterator<Integer> idIterator = this.idSet.iterator();
-		while (idIterator.hasNext()) {
-			int id = idIterator.next();
-			if (rc.canGetFlag(id)) {
-				int rawFlag = rc.getFlag(id);
-				Flag.IFlag flag = Flag.decode(rawFlag);
-				if (flag instanceof EnemySightedFlag) {
-					EnemySightedFlag enemySightedflag = (EnemySightedFlag)flag;
-					RobotType robotType = enemySightedflag.getRobotType();
-					MapLocation mapLoc = enemySightedflag.getMapLoc(rc.getLocation());
-					Target target = new Target(robotType, mapLoc);
-					if (this.targetFilter(target, rc)) {
-						this.targetQueue.add(target);
-						this.addedTargets.add(target);
-						UtilBattlecode.log("Added target @ " + target.mapLoc);
-					}
-				} else if ((this.targetQueue.size() > 0) && flag instanceof TargetMissingFlag) {
-					TargetMissingFlag targetMissingFlag = (TargetMissingFlag)flag;
-					MapLocation mapLoc = targetMissingFlag.getMapLoc(rc.getLocation());
-					if (mapLoc.equals(this.targetQueue.peek().mapLoc)) {
-						this.targetQueue.remove();
-					}
-				}
-			} else {
-				idIterator.remove();
-			}
-		}
-		
-		// select Target / Blueprint, build / deplay
-		if (this.buildNum % 2 == 1) {
-			if (this.attemptBuild(rc, new Blueprint(RobotType.SLANDERER, Assignment.PATROL), new PatrolAssignmentFlag(this.nextDegrees))) {
-				this.incrementDegrees();
-			}
-		}
-		else if (this.targetQueue.size() > 0) {
-			UtilBattlecode.log("HAS TARGET");
-			Target target = this.targetQueue.peek();
-			Blueprint blueprint = this.makeBlueprint(target);
-			AttackAssignmentFlag flag = new AttackAssignmentFlag(target.mapLoc);
-			this.attemptBuild(rc, blueprint, flag);
+		// listen for TargetMissingFlags/EnemySightedFlags
+		this.updateTargetQueue(rc);
+	
+		// did we recently build something?
+		if (this.flagCooldown > 0) {
+			// still need to keep its assignment flag up
+			this.flagCooldown--;
 		} else {
-			Blueprint blueprint = new Blueprint(RobotType.POLITICIAN, Assignment.PATROL);
-			PatrolAssignmentFlag flag = new PatrolAssignmentFlag(this.nextDegrees);
-			if (this.attemptBuild(rc, blueprint, flag)) {
-				this.incrementDegrees();
-			}
-			
+			this.attemptNextBuild(rc);
 		}
-        
+		
         return this;
 	}
 }
