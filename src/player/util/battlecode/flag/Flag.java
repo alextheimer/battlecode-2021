633a0283c08ad98e2d996d9891990104c1ca9937
Alex Theimer <theimer@mit.edu>
2021-02-05 19:22:15 -0500

    Added null checks to Flag.java.

--- src/player/util/battlecode/flag/Flag.java
+++ src/player/util/battlecode/flag/Flag.java
@@ -11,19 +11,22 @@ public class Flag {
 	public static int MAX_NUM_BITS = 24;  // Battlecode-imposed limit per flag
 	public static int EMPTY_FLAG = 0;
 
-	public static interface IFlag {
+	public interface IFlag {
 		// TODO(theimer): neither of these methods apply to client code
 
 		/**
 		 * Encodes the IFlag into a sequence of bits.
 		 * @return the bits of the encoded IFlag.
 		 */
-		public int encode();
+		int encode();
 		/**
 		 * Returns the number of bits used to encode the IFlag.
 		 */
-		public int numBits();
+		int numBits();
 	}
+
+	// TODO(theimer): the pre-contains and post-null checks below are cumbersome...
+	//    might be worth making a null-safe map class.
 
 	/**
 	 * Decodes the bits of an encoded IFlag into an IFlag instance.
@@ -34,11 +37,15 @@ public class Flag {
 	public static Flag.IFlag decode(final int rawFlag) {
 		assert UtilFlag.validBits(Flag.MAX_NUM_BITS, rawFlag) : "rawFlag: " + rawFlag;
 		final FlagWalker flagWalker = new FlagWalker(rawFlag);
+
 		// get the FlagOpCode
 		final int opCodeBits = flagWalker.readBits(UtilFlag.NUM_OP_CODE_BITS);
 		final UtilFlag.FlagOpCode opCode = UtilFlag.getOpCodeFromBits(opCodeBits);
+
 		// use the FlagOpCode to get the correct factory object
+		assert UtilFlag.opCodeToFactoryMap.containsKey(opCode) : "missing key: " + opCode;
 		final UtilFlag.IFlagFactory factory = UtilFlag.opCodeToFactoryMap.get(opCode);
+		assert factory != null : "map returned null";
 		final int flagBits = flagWalker.readBits(factory.numBits());
 		return factory.decode(flagBits);
 	}
@@ -47,7 +54,12 @@ public class Flag {
 	 * Returns the IFlag as an encoded sequence of bits.
 	 */
 	public static int encode(final Flag.IFlag flag) {
+		assert UtilFlag.flagToOpCodeMap.containsKey(flag.getClass()) : "missing key: " + flag.getClass();
+
 		final UtilFlag.FlagOpCode opCode = UtilFlag.flagToOpCodeMap.get(flag.getClass());
+
+		assert opCode != null : "map returned null";
+
 		final int opCodeBits = UtilFlag.getBitsFromOpCode(opCode);
 		final FlagWalker flagWalker = new FlagWalker(Flag.EMPTY_FLAG);
 		// write the FlagOpCode to facilitate later decoding
